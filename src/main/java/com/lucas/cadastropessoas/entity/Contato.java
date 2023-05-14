fffd940ce26deb5d62d7bd4f44365d8f3befc4e5
lucastanh <lucasvinicius564@hotmail.com>
2023-05-14 14:48:04 -0300

    feat: create Contato class

---
+++ src/main/java/com/lucas/cadastropessoas/entity/Contato.java
@@ -0,0 +1,25 @@
+package com.lucas.cadastropessoas.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String nome;
+
+    @Column(length = 11, nullable = false)
+    private String telefone;
+
+    @Column(length = 255, nullable = false)
+    private String email;
+}
